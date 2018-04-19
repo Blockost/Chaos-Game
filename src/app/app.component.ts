cfab29d557bbefcd7597c31c99a7f7eee3a624cf
Blockost <simon.espigole@gmail.com>
2018-04-19 04:02:24 +0100

    Start creating canvas component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  @ViewChild('canvas') canvasRef: ElementRef;
+
+  ngOnInit(): void {
+
+    let ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
+
+  }
 }
